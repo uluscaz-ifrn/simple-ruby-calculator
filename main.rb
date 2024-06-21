6b4f53a1687aec34de9531ba889e68c8a79b0f20
dantaslucas2 <33563024-dantaslucas2@users.noreply.replit.com>
2024-06-21 00:30:21 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,26 @@
+def calculadora
+  puts "Qual é o seu nome?"
+  nome = gets.chomp
+
+  puts "Olá #{nome}, digite o primeiro número:"
+  num1 = gets.chomp.to_f
+
+  puts "Digite o segundo número:"
+  num2 = gets.chomp.to_f
+
+  puts "A soma dos números #{num1} e #{num2} é: #{num1 + num2}"
+  puts "A subtração dos números #{num1} e #{num2} é: #{num1 - num2}"
+  puts "A multiplicação dos números #{num1} e #{num2} é: #{num1 * num2}"
+  puts "A divisão dos números #{num1} e #{num2} é: #{num1 / num2}"
+end
+
+puts "Olá, você deseja executar a calculadora?"
+puts "Digite 1 para sim e 2 para não"
+
+opcao = gets.chomp.to_i
+
+if opcao == 1
+  calculadora
+else
+  puts "Ok, até mais!"
+end
